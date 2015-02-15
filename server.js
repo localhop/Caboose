db3d9a99e520507f774b9e2b314d91ddc2c949b5
Kendal Harland <kharland2011@gmail.com>
2015-02-15 11:12:34 -0600

    added package.json

--- server.js
+++ server.js
@@ -11,23 +11,23 @@ if (process.argv.length < 5) {
   process.exit(1);
 }
 
-var express  = require('express'),
-		app    = express(),
-    mysql    = require('mysql'),
+var express    = require('express'),
+    mysql      = require('mysql'),
     bodyParser = require('body-parser'); // for POST 
+    app        = express(),
+    connpool = mysql.createPool({
+    	host : 'localhost',
+    	user : process.argv[2],
+    	password : process.argv[3],
+    	database : process.argv[4]
+    });
 
-var connectionpool = mysql.createPool({
-	host   : 'localhost',
-	user   : process.argv[2],
-	password : process.argv[3],
-	database : process.argv[4],
-});
-
+app.set('port', 3000);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/', function(req, res, next) {
 	res.type('json'); // This server returns only JSON requests
 });
-app.set('port', 3000);
+
 
 ///////////////////////////////////////////////////////////////////////////////
 /// Utility functions
@@ -46,19 +46,13 @@ function _debug(m) { console.log('> debug: ', m); }
 function handleMysqlConnErr(err, res) {
   _error(err);
   res.statusCode = 503;
-  res.send({
-    text: '',
-    error: err
-  });
+  res.send({text: '', error: err});
 }
 
 function handleMysqlQueryErr(err, res) {
   _error(err);
   res.statusCode = 500;
-  res.send({
-    text: '',
-    error: err
-  });
+  res.send({text: '', error: err});
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -76,14 +70,14 @@ app.get('/:nodeTest.js', function (req, res) {
 });
 
 app.get('/get/user/events/:userid', function(req, res) {
-  connectionpool.getConnection(function (err, connection) {
+  connpool.getConnection(function (err, conn) {
     if (err) {
       handleMysqlConnErr(err, res);
     } else {
       qstr = "call getUserEvents(?);";
       args = [req.params.userid];
-      connection.query(qstr, args, function(err, rows, fields) {
-      	connection.release();
+      conn.query(qstr, args, function(err, rows, fields) {
+      	conn.release();
         if (err) {
           handleMysqlQueryErr(err, res);
         } else {
@@ -95,14 +89,14 @@ app.get('/get/user/events/:userid', function(req, res) {
 });
 
 app.get('/create/user/group/:userid/:group', function (req, res) {
-  connectionpool.getConnection(function (err, connection) {
+  connpool.getConnection(function (err, conn) {
     if (err) {
       handleMysqlConnErr(err, res);
     } else {
       qstr = "call createUserGroup(?,?);";
       args = [req.params.userid, req.params.group];
-      connection.query(qstr, args, function(err, rows, fields) {
-      	connection.release();
+      conn.query(qstr, args, function(err, rows, fields) {
+      	conn.release();
         if (err) {
           handleMysqlQueryErr(err, res);
         } else {
