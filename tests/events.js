2680e4cd975814a288b322925d5495480e67a718
Kendal Harland <kharland2011@gmail.com>
2015-02-25 21:21:27 -0600

    added folder for unit tests

---
+++ tests/events.js
@@ -0,0 +1,8 @@
+var request = require('request');
+
+/* -------------- Event Tests ------------------*/
+
+var localhost  = "http://localhost:3000/"
+var remotehost = ""; 
+// Create Event
+request('http://www')
