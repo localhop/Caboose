f796223b06b99eecdc99bb0bb5fc034aac03c8a6
Kendal Harland <kharland2011@gmail.com>
2015-02-25 21:40:37 -0600

    create event working

--- tests/events.js
+++ tests/events.js
@@ -1,8 +1,23 @@
 var request = require('request');
-
-/* -------------- Event Tests ------------------*/
-
 var localhost  = "http://localhost:3000/"
 var remotehost = ""; 
-// Create Event
-request('http://www')
+
+function callback(error, response, body) {
+  if (!error  && response.statusCode == 200) {
+    console.log(body);
+  } else {
+    console.log(error);
+  }
+}
+
+/** Create Event */
+var testEvent = {
+    name: "TestEvent", 
+    description: "This is a test event",
+    location: "1603 West 15th Street. Jayhawker Towers Apt. A Room 211",
+    inviteSetting: '1',
+    start: "2015-03-01",
+    end: "2015-03-02",
+    userid: "1"};
+
+request.post(localhost+"create/event", callback).form(testEvent);
